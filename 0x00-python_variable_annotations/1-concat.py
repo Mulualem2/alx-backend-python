f72b5656c0321306c0b4d79d0fa891a2989e9936
Mulualem2 <mulualemasfaw2321@gmail.com>
2023-01-05 15:37:04 +0000

    task 2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""defines a function that concats two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """returns a concatinated string"""
+
+    return (str1 + str2)
