544570c395cf5293390757e6f381b5d7593792ea
Mulualem2 <mulualemasfaw2321@gmail.com>
2023-01-05 19:29:54 +0000

    task 10

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" Annotate the below function and its parameters
+""" Annotate the below function’s parameters and
+    return values with the appropriate types.
     def element_length(lst):
         return [(i, len(i)) for i in lst] """
 from typing import Iterable, Sequence, List, Tuple
