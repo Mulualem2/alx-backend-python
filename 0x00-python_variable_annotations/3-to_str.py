70227e1eedfb81bee369629b4e55ecf189d6b8b8
Mulualem2 <mulualemasfaw2321@gmail.com>
2023-01-05 15:54:35 +0000

    task 4

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""defines  a function that returns a string form of the input"""
+
+
+def to_str(n: float) -> str:
+    """casts the input to string and return the string"""
+
+    return str(n)
