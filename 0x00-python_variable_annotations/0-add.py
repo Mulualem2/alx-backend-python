9a9e6dc6545cde157df44d946b6ba4eb40531d77
Mulualem2 <mulualemasfaw2321@gmail.com>
2023-01-05 15:24:39 +0000

    task 1

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,7 +2,8 @@
 """adds two float numbers"""
 
 def add(x: float, y: float) -> float:
-    """A function which retrun sum 
+    """A function which retrun sum
     of two float numbers
     """
+    
     return x + y
